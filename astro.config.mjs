d60279060e6829a7235e15556b24f247f9257332
André Valle Ortega <andrevalleortega@MacBook-de-Andre.local>
2026-04-22 19:35:00 -0600

    Configuración Hybrid: El fin de los errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import clerk from '@clerk/astro';
-
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Esto le dice a Astro: "Todo es estático (rápido), 
+  // EXCEPTO el portal que es dinámico (seguro)".
+  output: 'hybrid',
+
   integrations: [clerk()],
+  
+  // 2. Conecta el cerebro de Netlify
   adapter: netlify()
 });
