f7f2724dd3048e00da51f59a50210ec813269fb8
André Valle Ortega <andrevalleortega@MacBook-de-Andre.local>
2026-04-23 10:59:42 -0600

    Fix: Escudos activados para window y prerender configurado

--- .netlify/build/chunks/astro_scripts/before-hydration.js_C4_Dwmbx.mjs
+++
@@ -1,2 +0,0 @@
-import { r as runInjectionScript } from "../index_COh8w1AS.mjs";
-await runInjectionScript({ "sdkMetadata": { "version": "3.0.19", "name": "@clerk/astro", "environment": "production" } });
--- .netlify/v1/functions/ssr/.netlify/build/entry.mjs
+++
@@ -1,4 +0,0 @@
-import { c } from "./chunks/ssr-function_0wDioTTl.mjs";
-export {
-  c as createHandler
-};
